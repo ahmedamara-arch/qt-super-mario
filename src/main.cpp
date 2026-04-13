f7035e23fcd1542b012143da8501f36b6f357692
Ahmed <ahmedamara@aucegypt.edu>
2026-04-13 20:44:19 +0200

    Show You lost! message when player falls off screen

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,8 @@
 #include <QGraphicsRectItem>
 #include <QGraphicsScene>
 #include <QGraphicsView>
+#include <QMessageBox>
 #include <QTimer>
-
 #include "player.hpp"
 
 int main(int argc, char* argv[]) {
@@ -21,16 +21,35 @@ int main(int argc, char* argv[]) {
   ground.setPos(100, 250);
   scene.addItem(&ground);
 
+  QGraphicsRectItem platform2(0, 0, 200, 20);
+  platform2.setBrush(Qt::darkGreen);
+  platform2.setPos(500, 170);
+  scene.addItem(&platform2);
+
+  QGraphicsRectItem platform3(0, 0, 200, 20);
+  platform3.setBrush(Qt::darkGreen);
+  platform3.setPos(800, 100);
+  scene.addItem(&platform3);
+
   QGraphicsView view(&scene);
   view.setWindowTitle("Qt Super Mario");
   view.setFixedSize(640, 480);
   view.setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+  view.setFocusPolicy(Qt::NoFocus);
   view.show();
+
+  scene.setFocusItem(&player);
 
   QTimer timer;
   QObject::connect(&timer, &QTimer::timeout, &player, &Player::updateState);
   QObject::connect(&timer, &QTimer::timeout,
-                   [&view, &player]() { view.centerOn(&player); });
+                   [&view, &player, &timer]() {
+                     view.centerOn(&player);
+                     if (player.y() > 400) {
+                       timer.stop();
+                       QMessageBox::information(nullptr, "Game Over", "You lost!");
+                     }
+                   });
   timer.start(33);
 
   return app.exec();
